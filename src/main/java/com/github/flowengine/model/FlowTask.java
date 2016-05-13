29bba170b4d63512af15e267f7ba9f27490b1454
badqiu <badqiu@gmail.com>
2016-05-13 11:40:21 +0800

    1

--- src/main/java/com/github/flowengine/model/FlowTask.java
+++ src/main/java/com/github/flowengine/model/FlowTask.java
@@ -264,7 +264,7 @@ public class FlowTask extends FlowTaskDef<FlowTask> implements Comparable<FlowTa
 	protected void beforeExec(FlowContext context2) {
 	}
 
-	private synchronized void execSelf(boolean execParents,final FlowContext context) throws InstantiationException,
+	private synchronized void execSelf(boolean execParents, final FlowContext context) throws InstantiationException,
 			IllegalAccessException, ClassNotFoundException,
 			InterruptedException, IOException {
 		//判断所有父亲是否已完全执行
@@ -274,13 +274,9 @@ public class FlowTask extends FlowTaskDef<FlowTask> implements Comparable<FlowTa
 		if(isEnabled() == null || !isEnabled()) {
 			throw new RuntimeException("task no enabled,taskId:"+getTaskId());
 		}
-		if(execStartTime != null) {
-			logger.info("already exec "+this+", exit");
-			return;
-		}
 		
-		execStartTime = new Date();
 		TaskExecutor executor = lookupTaskExecutor(context);
+		execStartTime = new Date();
 		evalGroovy(context,getBeforeGroovy());
 		try {
 			while(true) {
