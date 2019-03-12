1890e372ce85c4fa21b849ec6bf6368f25f7ec7d
badqiu <badqiu@gmail.com>
2019-03-12 11:51:36 +0800

    add name lock

--- src/main/java/com/github/flowengine/util/NamedLockUtil.java
+++ src/main/java/com/github/flowengine/util/NamedLockUtil.java
@@ -12,27 +12,41 @@ public class NamedLockUtil {
 
 	static Map<String,Lock> locks = new ConcurrentHashMap<String,Lock>();
 	
+	public static void lock(String lockId) {
+		lock("default",lockId);
+	}
+	
 	public static void lock(String lockGroup,String lockId) {
 		Lock lock = getLock(lockGroup, lockId);
 		lock.lock();
 	}
 
+	public static void unlock(String lockId) {
+		unlock("default",lockId);
+	}
+	
 	public static void unlock(String lockGroup,String lockId) {
 		Lock lock = getLock(lockGroup, lockId);
 		lock.unlock();
+		locks.remove(getLockKey(lockGroup, lockId));
 	}
 	
 	public static synchronized Lock getLock(String lockGroup,String lockId) {
 		Assert.hasText(lockGroup,"lockGroup must be not empty");
 		Assert.hasText(lockId,"lockId must be not empty");
 		
-		String lockKey = lockGroup+"@"+lockId;
+		String lockKey = getLockKey(lockGroup, lockId);
 		Lock lock = locks.get(lockKey);
 		if(lock == null) {
 			lock = new ReentrantLock();
 			locks.put(lockKey, lock);
 		}
 		return lock;
+	}
+
+	private static String getLockKey(String lockGroup, String lockId) {
+		String lockKey = lockGroup+"@"+lockId;
+		return lockKey;
 	}
 
 }
---
+++ src/test/java/com/github/flowengine/util/NamedLockUtilTest.java
@@ -0,0 +1,27 @@
+package com.github.flowengine.util;
+
+import org.apache.hadoop.util.ThreadUtil;
+import org.junit.Test;
+
+public class NamedLockUtilTest {
+
+	@Test
+	public void test() throws InterruptedException {
+		
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				NamedLockUtil.lock("badqiu_lock");
+				System.out.println("get lock on thread");
+				ThreadUtil.sleepAtLeastIgnoreInterrupts(1000 * 10);
+				NamedLockUtil.unlock("badqiu_lock");
+			}
+		}).start();;
+		
+		ThreadUtil.sleepAtLeastIgnoreInterrupts(1000);
+		
+		NamedLockUtil.lock("badqiu_lock");
+		System.out.println("END");
+	}
+
+}
